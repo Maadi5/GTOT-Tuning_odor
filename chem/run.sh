19304473113db8b604135369df533140a5078016
Maadi5 <ma.adithya55@gmail.com>
2023-04-02 15:54:55 +0530

    update dataset path

--- chem/run.sh
+++ chem/run.sh
@@ -16,7 +16,7 @@ gtot_order=1
 
 
 dataset=odour
-python ${data_path}/finetune.py --data_path ./chem/${data_path} --input_model_file ${data_path}/model_gin/contextpred.pth --split scaffold --gpu 0 --runseed 0 --gnn_type gin --dataset ${dataset} --gtot_order ${gtot_order} --regularization_type ${rg_type} --finetune_type ${ft_type} --attention_epochs 10 --attention_iteration_limit 10 --tag ${tag} --save_file ${save_file} --patience ${patience} --dist_metric ${dist_metric} --lr 0.005 --dropout_ratio 0.3 --trade_off_backbone 0.005 --trade_off_head 0.0 --decay 1e-7 --batch_size 32 --filename ${filename}
+python ${data_path}/finetune.py --data_path ${data_path} --input_model_file ${data_path}/model_gin/contextpred.pth --split scaffold --gpu 0 --runseed 0 --gnn_type gin --dataset ${dataset} --gtot_order ${gtot_order} --regularization_type ${rg_type} --finetune_type ${ft_type} --attention_epochs 10 --attention_iteration_limit 10 --tag ${tag} --save_file ${save_file} --patience ${patience} --dist_metric ${dist_metric} --lr 0.005 --dropout_ratio 0.3 --trade_off_backbone 0.005 --trade_off_head 0.0 --decay 1e-7 --batch_size 32 --filename ${filename}
 exit
 dataset=bace
 python ${data_path}/finetune.py --input_model_file ${data_path}/model_gin/contextpred.pth --split scaffold --gpu 0 --runseed 0 --gnn_type gin --dataset ${dataset} --gtot_order ${gtot_order} --regularization_type ${rg_type} --finetune_type ${ft_type} --attention_epochs 10 --attention_iteration_limit 10 --tag ${tag} --save_file ${save_file} --patience ${patience} --dist_metric ${dist_metric} --lr 0.0005 --dropout_ratio 0.25 --trade_off_backbone 0.01 --trade_off_head 0.0001 --decay 0.0005 --batch_size 16 --filename ${filename}
