795f9510ad30d280b8d4fab74e342b89c6ea5dbb
Maadi5 <ma.adithya55@gmail.com>
2023-04-09 19:56:47 +0530

    added prints for prediction and gt

--- chem/finetune.py
+++ chem/finetune.py
@@ -348,10 +348,23 @@ def train_epoch(args, model, device, loader, optimizer, weights_regularization, 
 
             # intermediate_output_s
 
-        print('prediction: ', pred.tolist())
+        print('prediction: ')
+        for p in pred.tolist():
+            plist = []
+            for idx, po in enumerate(p):
+                if po>0.5:
+                    plist.append(odours[idx])
+            print(plist)
 
         y = batch.y.view(pred.shape).to(torch.float64)
-        print('gt: ', ((y + 1) / 2).tolist())
+        print('gt: ')
+        for p in ((y + 1) / 2).tolist():
+            plist = []
+            for idx, po in enumerate(p):
+                if po>0.5:
+                    plist.append(odours[idx])
+            print(plist)
+
         # Whether y is non-null or not.
         is_valid = y ** 2 > 0
         # Loss matrix
